bd522b487d1285b67891d6b77e2b8604e65cfddd
zhivaijkin-daniil <lookold48@gmail.com>
2020-05-24 23:54:46 +0300

    no final

---
+++ zhivaijkin-daniil/bypass/bypass/main.cpp
@@ -0,0 +1,14 @@
+#include "binTree.h"
+#include <time.h>
+
+int main()
+{
+	int n = 0;
+	srand(time(0));
+	Node cpy();
+	BinTree bt(&node);
+	cout << "versh" << endl;
+	cin >> n;
+	for(i = 0; i < n; i++)
+
+}
